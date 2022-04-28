9922f424e7748b259d2113d6fd1e109e0b0a1cf3
JulieHaguait <jhaguait@gmail.com>
2022-04-28 17:36:46 +0200

    create repository for SkyKid, Devise,Admin

---
+++ SkyMaven/src/main/java/SkyMaven/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package SkyMaven.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SkyMaven.entity.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+}
