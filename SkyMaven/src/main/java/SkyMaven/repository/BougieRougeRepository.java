8f987f2e55c74640678ad8604dd7d6037bff9395
taifourhamza <taifour.hamza@gmail.com>
2022-05-02 09:29:02 +0200

    Création repositories : BougieRouge / BougieBlanche / Coeur / Item/ Node

---
+++ SkyMaven/src/main/java/SkyMaven/repository/BougieRougeRepository.java
@@ -0,0 +1,9 @@
+package SkyMaven.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SkyMaven.entity.BougieRouge;
+
+public interface BougieRougeRepository extends JpaRepository<BougieRouge, Long> {
+
+}
---
+++ SkyMaven/src/main/java/SkyMaven/repository/CoeurRepository.java
@@ -0,0 +1,9 @@
+package SkyMaven.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SkyMaven.entity.Coeur;
+
+public interface CoeurRepository extends JpaRepository<Coeur, Long> {
+
+}
