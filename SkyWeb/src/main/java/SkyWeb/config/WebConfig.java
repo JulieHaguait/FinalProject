4adf2049e367cd4fcb2e03c18cd8d8dcc27f3200
taifourhamza <taifour.hamza@gmail.com>
2022-05-19 10:36:26 +0200

    Config

--- SkyWeb/src/main/java/SkyWeb/config/WebConfig.java
+++ SkyWeb/src/main/java/SkyWeb/config/WebConfig.java
@@ -1,5 +1,30 @@
 package SkyWeb.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import SkyMaven.config.SkyConfig;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"SkyMaven.restController"})
+@Import(SkyConfig.class)
 public class WebConfig {
 
-}
+	
+
+		@Bean
+		public UrlBasedViewResolver viewResolver() {
+			UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+			viewResolver.setViewClass(JstlView.class);
+			viewResolver.setPrefix("/WEB-INF/pages/");
+			viewResolver.setSuffix(".jsp");
+			return viewResolver;
+		}
+	}
